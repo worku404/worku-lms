1e60b885f902dbe6759f64ef4838df70a7c917cc
worku404 <worku.wondoson@aastustudent.edu.et>
2026-02-15 16:57:22 +0300

    feat(courses): add drag-and-drop module ordering in content manager
    Update module content template and base template JS blocks to support sortable modules, wire ordering endpoint in URLs, and implement server-side order update handling in views.

--- edu/courses/views.py
+++ edu/courses/views.py
@@ -9,6 +9,9 @@ from django.apps import apps
 from django.forms.models import modelform_factory
 from django.http import Http404
 
+
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+
 from .models import Module,Course, Content
 from .forms import ModuleFormSet
 
@@ -248,3 +251,21 @@ class ModuleContentListView(TemplateResponseMixin, View):
             course__owner=request.user
         )
         return self.render_to_response({'module': module})
+    
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(
+                id=id,
+                course__owner=request.user
+            ).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+    
+class ContentOrderview(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(
+                id=id,
+                module__course__owner = request.user
+            ).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
