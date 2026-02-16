e095a20b7d9472ecbb6fdeb74e7f1b359a812d3a
worku404 <worku.wondoson@aastustudent.edu.et>
2026-02-16 21:10:39 +0300

    Fix LLM waiting animation

--- edu/edu/settings.py
+++ edu/edu/settings.py
@@ -139,6 +139,12 @@ CACHES = {
     }
 }
 
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
+#         "LOCATION": "127.0.0.1:11211",
+#     }
+# }
 # Development local IPs (used by debug-toolbar)
 INTERNAL_IPS = ["127.0.0.1"]
 
