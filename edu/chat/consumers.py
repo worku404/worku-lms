e8e8c22f8f5eddc7e4727febe0a1d3ffd5cb3840
worku404 <worku.wondoson@aastustudent.edu.et>
2026-02-18 13:50:30 +0300

    add fully functional course chat room

--- edu/chat/consumers.py
+++ edu/chat/consumers.py
@@ -1,13 +1,14 @@
 import json
 from channels.generic.websocket import  AsyncWebsocketConsumer
-from asgiref.sync import async_to_sync
 from django.utils import timezone
+
+from chat.models import Message
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.user = self.scope['user']
-        self.id = self.scope['url_route']['kwargs']['course_id']
-        self.room_group_name = 'chat_%s' % self.id
+        self.course_id = self.scope['url_route']['kwargs']['course_id']
+        self.room_group_name = 'chat_%s' % self.course_id
         #join room group
         await self.channel_layer.group_add(
             self.room_group_name, self.channel_name
@@ -20,7 +21,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_discard (
             self.room_group_name, self.channel_name
         )
+        
+    async def persist_message(self, message):
+    #send message to WebSocket
     
+        await Message.objects.acreate(
+            user=self.user,
+            course_id=self.course_id,
+            content=message
+        )
     #Recieve message from Websocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -36,9 +45,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 'datetime': now.isoformat(),
             }
         )
-        
+        #persist message
+        await self.persist_message(message)
         #receive message from room group
     async def chat_message(self, event):
         #send message to wobsocket
         
         await self.send(text_data=json.dumps(event))
+    
+
+    
