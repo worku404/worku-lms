23835aff085e404d54688f01f812eb2b86ec0f37
worku404 <worku.wondoson@aastustudent.edu.et>
2026-03-27 15:27:43 +0300

    assistant: register chat models in admin
    Add list views for AssistantChat and AssistantTurn.

--- edu/assistant/admin.py
+++ edu/assistant/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import AssistantChat, AssistantTurn
+
+
+@admin.register(AssistantChat)
+class AssistantChatAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "title", "is_pinned", "pinned_at", "updated_at")
+    list_filter = ("is_pinned",)
+    search_fields = ("title", "user__username", "user__email")
+    ordering = ("-updated_at",)
+
+
+@admin.register(AssistantTurn)
+class AssistantTurnAdmin(admin.ModelAdmin):
+    list_display = ("id", "chat", "created_at")
+    search_fields = ("prompt", "response")
+    ordering = ("-created_at",)
