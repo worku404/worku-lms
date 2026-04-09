7a301f6a99e1f824f57e97b7140972dfd257b1ac
worku404 <worku.wondoson@aastustudent.edu.et>
2026-04-09 19:02:47 +0300

    Refresh note search index on tag changes

--- edu/notes/signals.py
+++ edu/notes/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_delete, post_save
+from django.db.models.signals import m2m_changed, post_delete, post_save
 from django.dispatch import receiver
 
 from .models import Note, Tag
@@ -20,3 +20,9 @@ def refresh_notes_for_tag(sender, instance, **kwargs):
     note_ids = list(instance.notes.values_list("id", flat=True))
     for note_id in note_ids:
         refresh_note_search_index(note_id)
+
+
+@receiver(m2m_changed, sender=Note.tags.through)
+def refresh_note_index_on_tag_change(sender, instance, action, **kwargs):
+    if action in ("post_add", "post_remove", "post_clear"):
+        refresh_note_search_index(instance.id)
